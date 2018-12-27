1f33d4c3d90392c42deebfc8828f781dffe28460
Timothy Brink <timbr2014@gmail.com>
2018-12-27 17:04:17 -0500

    rename for clarity

--- src/HitRegion.js
+++ src/HitRegion.js
@@ -28,8 +28,8 @@ module.exports = class HitRegion extends Shape {
   listen() {
     if (!this.context) throw new Error('Context is not defined!');
 
-    this.handlers.forEach(h => {
-      if (h.eventType === 'c2d-mouseinout') {
+    this.handlers.forEach(handler => {
+      if (handler.eventType === 'c2d-mouseinout') {
         this.context.canvas.addEventListener('mousemove', e => {
 
           if (typeof window._c2dHandlerWasIn === 'undefined') {
@@ -41,17 +41,17 @@ module.exports = class HitRegion extends Shape {
           let isIn = (e.x > pos.x && e.x < pos.x + this.width && e.y > pos.y && e.y < pos.y + this.height);
 
           if (window._c2dHandlerWasIn && !isIn) {
-            h.handler(e, 'mouseout');
+            handler.handler(e, 'mouseout');
           } else if (!window._c2dHandlerWasIn && isIn) {
-            h.handler(e, 'mousein');
+            handler.handler(e, 'mousein');
           }
           window._c2dHandlerWasIn = isIn;
         });
       } else {
-        this.context.canvas.addEventListener(h.eventType, e => {
+        this.context.canvas.addEventListener(handler.eventType, e => {
           let pos = this.getPos();
           if (e.x > pos.x && e.x < pos.x + this.width && e.y > pos.y && e.y < pos.y + this.height) {
-            h.handler(e);
+            handler.handler(e);
           }
         });
       }
