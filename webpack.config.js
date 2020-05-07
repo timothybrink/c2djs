b958d47f6d9360f3e1395e0afe8c07dab84df291
Timothy Brink <contact@timothybrink.dev>
2020-05-07 15:16:20 -0400

    update deps

--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,10 @@ const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  entry: path.resolve(__dirname, 'src/index.js'),
+  entry: './src/index.js',
   output: {
     filename: 'c2d.js',
     path: path.resolve(__dirname, 'dist')
-  },
-  module: {
-    loaders: [
-      {
-        loader: 'babel-loader',
-        test: path.resolve(__dirname, 'src')
-      }
-    ]
   },
   plugins: [
     new CopyWebpackPlugin([{from: path.resolve(__dirname, 'test')}])
@@ -21,5 +13,6 @@ module.exports = {
   devtool: 'source-map',
   devServer: {
     contentBase: path.resolve(__dirname, 'dist'),
-  }
+  },
+  mode: 'development',
 };
